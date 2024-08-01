4d74ebf81e5e04fef3c615189655d9c2a4b85d82
vinay <vinaysindiit@gmail.com>
2024-08-01 09:34:18 +0530

    changes

--- src/auth/Login.jsx
+++ src/auth/Login.jsx
@@ -12,6 +12,7 @@ export default function Login() {
   const navigate = useNavigate();
   const [password, setPassword] = useState("");
   const [errors, setErrors] = useState({});
+
   const [loading, setLoading] = useState(false);
   const togglePasswordVisibility = () => {
     setShowPassword(!showPassword);
