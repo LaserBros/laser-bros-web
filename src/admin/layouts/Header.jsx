54a27be0beea4525762da0c11c9959a275c29964
Vinay <apple@Vinays-MacBook-Pro.local>
2025-05-08 15:03:57 +0530

    Remove dark mode

--- src/admin/layouts/Header.jsx
+++ src/admin/layouts/Header.jsx
@@ -44,7 +44,7 @@ const Header = ({ title }) => {
             className="headerright"
             style={{ display: "inline-flex", alignItems: "center" }}
           >
-            <ThemeToggle />
+            {/* <ThemeToggle /> */}
             <NavDropdown
              className="ms-2"
               title={
